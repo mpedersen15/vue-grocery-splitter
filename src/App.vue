b41450444affa42565f5e4652942f1e020430a24
mpedersen <matthewjoelpedersen@gmail.com>
2019-03-19 20:57:41 -0600

    add TransactionList component

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,40 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TransactionList
+      v-bind:transactions="[
+      {
+        id: 1,
+        user: 1,
+        amount: 15.5
+      },
+      {
+        id: 2,
+        user: 2,
+        amount: 10.0
+      }
+    ]"
+    />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import TransactionList from "./components/TransactionList.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    HelloWorld,
+    TransactionList
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/TransactionList.vue
@@ -0,0 +1,21 @@
+<template>
+  <ul class="transaction-list">
+    <li
+      v-for="transaction in transactions"
+      v-bind:key="transaction.id"
+    >{{transaction.user + " - " + transaction.amount}}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "TransactionList",
+  props: {
+    transactions: Array
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
